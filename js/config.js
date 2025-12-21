5feb13c42aa2f2ed2bec3c9472a71435bab5c311
heroyik <heroyik@gmail.com>
2025-12-21 16:59:40 +0900

    feat: Add Supabase High Score implementation

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const SUPABASE_CONFIG = {
+    url: 'https://wcljwdhggezifxdlsjdj.supabase.co',
+    key: 'sb_publishable_6sZGLca_uZDR28RNZHhwEw_ISYr0bO_'
+};
+
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
